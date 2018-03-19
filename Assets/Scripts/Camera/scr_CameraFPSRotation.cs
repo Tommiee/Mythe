6f92db394fa03a0080a1f404bdc03ec914889f9f
Tommiee <tomdepom1999@gmail.com>
2018-03-19 10:02:08 +0100

    Update files
    Semi-merge

--- Assets/Scripts/Camera/scr_CameraFPSRotation.cs
+++ Assets/Scripts/Camera/scr_CameraFPSRotation.cs
@@ -5,30 +5,58 @@ using UnityEngine;
 public class scr_CameraFPSRotation : MonoBehaviour {
 
 	[SerializeField]
-	float cameraSpeed;
+	private float cameraSpeed = 2;
+    [SerializeField]
+    private float limitAngle = 80f;
 	[SerializeField]
-	GameObject parent;
+	private GameObject parent;
 
+    private float cameraRotationX = 0f;
+    private float currentCamRotX = 0;
+    private Vector3 rotation = Vector3.zero;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(transform.rotation.x > -80 && transform.rotation.x < 80)
-		this.transform.Rotate(GetYRot(), 0, 0);
-        parent.transform.Rotate(0, GetXRot(), 0);
-	}
+        
+        //float _cameraRotationX = GetXRot() * cameraSpeed;
+        
+        parent.transform.Rotate(0, GetYRot(), 0);
+        this.transform.Rotate(GetXRot(), 0, 0);
 
+        //print(this.transform.localEulerAngles.x);
 
+        rotation = new Vector3(Mathf.Clamp(this.transform.localEulerAngles.x, -limitAngle, limitAngle), 0, 0);
+        print(rotation.x);
+        if(rotation.x >= 0)
+        {
+            this.transform.localEulerAngles = rotation;
+        } else
+        {
+            this.transform.localEulerAngles = new Vector3(360 - rotation.x, 360 - rotation.y, 360 - rotation.z);
+        }
+        
+        /*
+        rotation = new Vector3(0f, GetYRot(), 0f) * cameraSpeed;
+        parent.GetComponent<Rigidbody>().MoveRotation(transform.rotation * Quaternion.Euler(rotation));
+        currentCamRotX -= cameraRotationX;
+        currentCamRotX = Mathf.Clamp(currentCamRotX, -limitAngle, limitAngle);
+        //print("cam transform localEulerangles: " + this.transform.localEulerAngles.x);
+        //print("currentCamRotX: " + currentCamRotX);
+        //print("cameraRotationX: " + cameraRotationX);
+        this.transform.localEulerAngles = new Vector3(currentCamRotX, 0f, 0f);
+        */
+    }
 
-	public float GetXRot()
+	public float GetYRot()
 	{
 		return Input.GetAxis("Mouse X") * cameraSpeed;
 	}
-	public float GetYRot()
+	public float GetXRot()
 	{
 		return -Input.GetAxis("Mouse Y") * cameraSpeed;
 	}
@@ -49,4 +77,17 @@ public class scr_CameraFPSRotation : MonoBehaviour {
         {
             transform.Rotate(_rotation);
         }
+
+        void performRotation()
+    {
+        rb.MoveRotation(transform.rotation * Quaternion.Euler(rotation));
+        if(cam != null)
+        {
+            currentCamRotX -= cameraRotationX;
+            currentCamRotX = Mathf.Clamp(currentCamRotX, -camRotLimit, camRotLimit);
+
+            cam.transform.localEulerAngles = new Vector3(currentCamRotX, 0f, 0f);
+        }
+    }
+    
    */
---
+++ Assets/Scripts/Movement/scr_CharacterController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_CharacterController : MonoBehaviour {
+    private scr_InputManager inputManager;
+    private scr_GrabObject grabManager;
+
+    [SerializeField]
+    private float grabRange = 20f;
+    [SerializeField]
+    private float inputSpeed = 5;
+    [SerializeField]
+    private float sprintMultiplier = 2;
+    private float movementSpeed;
+    private bool movementLock;
+
+    void Start()
+    {
+        //Find and add required scripts, if you've forgotten/dont want to add them in the editor.
+        //Not completely necessary, but what the hell, i'm both lazy and forgetful.
+        if (!(inputManager = this.GetComponent<scr_InputManager>()))
+        {
+            inputManager = this.gameObject.AddComponent<scr_InputManager>();
+        }
+        if (!(grabManager = this.GetComponent<scr_GrabObject>()))
+        {
+            grabManager = this.gameObject.AddComponent<scr_GrabObject>();
+        }
+    }
+
+    void Update()
+    {
+        Walk();
+        Grab();
+    }
+
+    void Grab()
+    {
+        //It grabs stuff.
+        if (inputManager.F())
+        {
+            GameObject grabbed = grabManager.PickUp(grabRange);
+            Debug.Log(grabbed);
+        }
+    }
+
+    void Walk()
+    {
+        if (!movementLock)
+        {
+            //This bit moves the character.
+            Vector3 moveVector = new Vector3();
+            movementSpeed = inputSpeed;
+
+            if (inputManager.Shift())
+            {
+                movementSpeed = inputSpeed * sprintMultiplier;
+            }
+            if (inputManager.Up())
+            {
+                moveVector += this.transform.forward;
+            }
+            if (inputManager.Down())
+            {
+                moveVector -= this.transform.forward;
+            }
+            if (inputManager.Right())
+            {
+                moveVector += this.transform.right;
+            }
+            if (inputManager.Left())
+            {
+                moveVector -= this.transform.right;
+            }
+            moveVector.Normalize();
+            this.transform.position += (moveVector * Time.deltaTime * movementSpeed);
+        }
+    }
+
+    //These two are required for freezing the movement when opening the inventory.
+    //I know it looks odd, but we need these.
+    public void Freeze()
+    {
+        movementLock = true;
+    }
+
+    public void Unfreeze()
+    {
+        movementLock = false;
+    }
+}
